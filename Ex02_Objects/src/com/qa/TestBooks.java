96e974cdadeee7f788875c68fcb3110e49826a0d
Konstantinos Poulinakis <poulinakis.kon@gmail.com>
2023-07-25 16:51:52 +0100

    Saving Java Ex2

---
+++ Ex02_Objects/src/com/qa/TestBooks.java
@@ -0,0 +1,10 @@
+package com.qa;
+
+public class TestBooks {
+    public static void main(String[] args){
+        String arr[] = {"JR Tolkin"};
+        Book book1 = new Book("Lord of the Rings", arr,45);
+        System.out.println(book1);
+
+    }
+}
